5eea99a754e0ca59654f94b455e8f7611dd2b61f
cooldruid <vasko9911@gmail.com>
2026-02-22 16:12:19 +0100

    Initial board and movement

---
+++ src/client/fireholde-under-attack/src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have the 'fireholde-under-attack' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('fireholde-under-attack');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, fireholde-under-attack');
+  });
+});
---
+++ src/client/fireholde-under-attack/src/app/game/game.component.ts
@@ -0,0 +1,140 @@
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { Tile } from './data/tile';
+
+@Component({
+  selector: 'app-game',
+  standalone: true,
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.scss'
+})
+export class GameComponent {
+  board: Tile[] = [
+    {id: 1, type: 'start'},
+    {id: 2, type: 'shop'},
+    {id: 3, type: 'shop'},
+    {id: 4, type: 'shop'},
+    {id: 5, type: 'shop'},
+    {id: 6, type: 'shop'},
+    {id: 7, type: 'shop'},
+    {id: 8, type: 'shop'},
+    {id: 9, type: 'shop'},
+    {id: 10, type: 'shop'},
+    {id: 11, type: 'shop'},
+    {id: 12, type: 'shop'},
+    {id: 13, type: 'shop'},
+    {id: 14, type: 'shop'},
+    {id: 15, type: 'shop'},
+    {id: 16, type: 'shop'},
+    {id: 17, type: 'shop'},
+    {id: 18, type: 'shop'},
+    {id: 19, type: 'shop'},
+    {id: 20, type: 'shop'},
+    {id: 21, type: 'shop'},
+    {id: 22, type: 'shop'},
+    {id: 23, type: 'shop'},
+    {id: 24, type: 'shop'},
+    {id: 25, type: 'shop'},
+    {id: 26, type: 'shop'},
+    {id: 27, type: 'shop'},
+    {id: 28, type: 'shop'},
+    {id: 29, type: 'shop'},
+    {id: 30, type: 'shop'},
+    {id: 31, type: 'shop'},
+    {id: 32, type: 'shop'},
+    {id: 33, type: 'shop'},
+    {id: 34, type: 'shop'},
+    {id: 35, type: 'shop'},
+    {id: 36, type: 'shop'}
+  ]
+
+  @ViewChildren('tile') tiles!: QueryList<ElementRef>;
+
+  tileCoordinates: TilePosition[] = [];
+  tokenTransform: string = '';
+  player: Player = {
+    currentTile: 1
+  };
+
+  ngAfterViewInit() {
+    this.cacheTilePositions();
+    this.animateTo(1);
+  }
+
+  cacheTilePositions() {
+    this.tileCoordinates = this.tiles.map(tile => {
+      const rect = tile.nativeElement.getBoundingClientRect();
+
+      return {
+        x: rect.left,
+        y: rect.top
+      };
+    });
+  }
+
+  getGridPosition(index: number) {
+    const sideSize = 10;
+
+    if (index < sideSize) {
+      // bottom row
+      return { gridRow: sideSize, gridColumn: index % sideSize};
+    }
+    if (index < sideSize * 2 - 1) {
+      // right column
+      return { gridRow: 2 * sideSize - index, gridColumn: sideSize };
+    }
+    if (index < sideSize * 3 - 2) {
+      // top row
+      return { gridRow: 1, gridColumn: 3 * sideSize - index - 1 };
+    }
+    if (index < sideSize * 4 - 3) {
+      // left column
+      return { gridRow: (index % (3 * sideSize - 2)) + 1, gridColumn: 1 };
+    }
+
+    return {display: 'none'};
+  }
+
+  async movePlayer(squaresToMove: number) {
+    let current = this.player.currentTile;
+
+    const targetTile = this.player.currentTile + squaresToMove <= this.tiles.length ?
+      this.player.currentTile + squaresToMove :
+      squaresToMove - (this.tiles.length - this.player.currentTile);
+
+    while (current !== targetTile) {
+      current = current + 1;
+
+      if(current > this.tiles.length)
+        current = 1;
+
+      await this.animateTo(current);
+    }
+
+    this.player.currentTile = targetTile;
+  }
+
+  animateTo(index: number): Promise<void> {
+    return new Promise(resolve => {
+      const coords = this.tileCoordinates[index - 1];
+
+      this.tokenTransform =
+        `translate(${coords.x}px, ${coords.y}px)`;
+
+      setTimeout(resolve, 200);
+    });
+  }
+}
+
+interface TilePosition {
+  x: number;
+  y: number;
+}
+
+interface Player {
+  currentTile: number;
+}
+// 16 1/1
+// 17 2/1
+// 18 3/1
+// 19 4/1
+// 20 5/1
